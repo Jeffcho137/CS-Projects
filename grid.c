7838b79f9f67a27a4a1af91108848c828fc80871
Tanli Su 22 <tanlisu@flume.cs.dartmouth.edu>
2020-08-20 16:55:17 -0400

    start sudoku, add grid module

---
+++ grid.c
@@ -0,0 +1,219 @@
+/* 
+ * grid.c - sudoku 'grid' module
+ * see grid.h for more information.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "grid.h"
+
+/**************** file-local global variables ****************/
+/* none */
+
+/**************** global types ****************/
+typedef int** grid_t;   // 2D array representing 9 rows of 9 integers
+
+/**************** global functions ****************/
+/* that is, visible outside this file */
+/* see grid.h for comments about exported functions */
+
+/**************** local functions ****************/
+/* none */
+
+/**************** grid_new() ****************/
+/* see grid.h for description */
+grid_t *
+grid_new()
+{
+  grid_t* grid = count_malloc(9 * sizeof(int *));
+  if (grid == NULL) {
+    return NULL;              // error allocating grid
+  }
+
+  // initialize the contents of each row
+  for (int i = 0; i < 9; i++) {
+    grid[i] = count_malloc(9 * sizeof(int));
+    if (grid[i] == NULL) {
+        return NULL;          // error allocating grid
+    }
+
+    // fill row with 0s
+    for (int j = 0; j < 9; j++) {
+        grid[i][j] = 0;
+    }
+  }
+
+  return grid;
+}
+
+/**************** grid_get() ****************/
+/* see grid.h for description */
+int
+grid_get(grid_t* grid, int row, int col)
+{
+  if (grid == NULL) { // bad grid
+    return -1;
+  } else if (row < 0 || col < 0 || row > 9 || col > 9) { // bad row/col
+    return -1;
+  } else {
+    return grid[row][col];
+  }
+}
+
+/**************** grid_set() ****************/
+/* see grid.h for description */
+bool
+grid_set(grid_t* grid, int row, int col, int num)
+{
+  if (grid == NULL) { // bad grid
+    return false;
+  } else if (row < 0 || col < 0 || row > 9 || col > 9) { // bad row/col
+    return false;
+  } else if (num < 0 || num > 9) { // bad num
+    return false;
+  } else {
+    grid[row][col] = num;
+  }
+
+#ifdef MEMTEST
+  count_report(stdout, "After grid_set");
+#endif
+}
+
+
+
+/**************** grid_print() ****************/
+/* see grid.h for description */
+void
+grid_print(grid_t* grid, FILE *fp)
+{
+  if (fp != NULL) {
+    if (grid != NULL) {
+      for (int i = 0; i < 9; i++) {
+        // print this row
+        for (int j = 0; j < 8; j++) {
+            fprintf(fp, "%d ", grid[i][j]);
+        }
+        fprintf(fp, "%d\n", grid[i][9]);
+      }
+    } else {
+      fputs("(null)", fp);
+    }
+  }
+}
+
+/**************** grid_delete() ****************/
+/* see grid.h for description */
+void 
+grid_delete(grid_t* grid)
+{
+  if (grid != NULL) {
+    for (int i = 0; i < 9; i++) {
+      count_free(grid[i]);
+    }
+    count_free(grid);
+  }
+
+#ifdef MEMTEST
+  count_report(stdout, "End of grid_delete");
+#endif
+}
+
+/**************** valid_grid() ****************/
+/* see grid.h for description */
+bool
+valid_grid(grid_t* grid)
+{
+  if (grid != NULL) {
+    // check that each row is valid
+    for (int i = 0; i < 9; i++) {
+        // keep track of numbers that have been found
+        bool found_num[9];
+        for (int j = 0; j < 9; j++) {
+            found_num[j] = false;
+        }
+        // check each number in the row
+        for (int j = 0; j < 9; j++) {
+            int num = grid[i][j];
+            if (num != 0) {
+                if (found_num[num]) {
+                    return false;
+                }
+                found_num[num] = true;
+            }
+        }
+    }
+
+    // check that each column is valid
+    for (int i = 0; i < 9; i++) {
+        // keep track of numbers that have been found
+        bool found_num[9];
+        for (int j = 0; j < 9; j++) {
+            found_num[j] = false;
+        }
+        // check each number in the column
+        for (int j = 0; j < 9; j++) {
+            int num = grid[j][i];
+            if (num != 0) {
+                if (found_num[num]) {
+                    return false;
+                }
+                found_num[num] = true;
+            }
+        }
+    }
+
+    // check that each 3x3 square is valid
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            // keep track of numbers that have been found
+            bool found_num[9];
+            for (int k = 0; k < 9; k++) {
+                found_num[k] = false;
+            }
+            // check each number in the grid
+            for (int row = 3 * i + 1; row < 3 * i + 4; row++) {
+                for (int col = 3 * j + 1; row < 3 * i + 4; row++) {
+                    int num = grid[row][col];
+                    if (num != 0) {
+                        if (found_num[num]) {
+                            return false;
+                        }
+                        found_num[num] = true;
+                    }
+                }
+            }
+        }
+    }
+  }
+}
+
+/**************** load_grid() ****************/
+/* see grid.h for description */
+grid_t *
+load_grid(FILE *fp)
+{
+  if (fp == NULL) {
+      return NULL;
+  }
+
+  grid_t* grid = grid_new();
+  if (grid != NULL) {
+    // read in each row
+    for (int i = 0; i < 9; i++) {
+        if (fscanf(fp, "%d %d %d %d %d %d %d %d %d",
+            &grid[i][0], &grid[i][1], &grid[i][2],
+            &grid[i][3], &grid[i][4], &grid[i][5],
+            &grid[i][6], &grid[i][7], &grid[i][8])
+            != 9) {
+            return NULL; // the row has invalid syntax
+        }
+    }
+    // check that the sudoku grid is valid
+    if (!valid_grid(grid)) {
+        return NULL;
+    }
+  }
+
+  return grid;
+}
---
+++ grid.h
@@ -0,0 +1,117 @@
+/* 
+ * grid.h - header file for sudoku grid module
+ *
+ * A suite of functions that implement the sudoku “grid” data structure;
+ * this module also includes functions to get a grid from stdin
+ * (used by solve.c), 
+ */
+
+#ifndef __GRID_H
+#define __GRID_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+/**************** global types ****************/
+typedef struct grid grid_t;  // opaque to users of the module
+
+/**************** functions ****************/
+
+/**************** grid_new ****************/
+/* Create a new (empty) grid.
+ *
+ * We return:
+ *   pointer to a new grid, or NULL if error.
+ * We guarantee:
+ *   The grid is initialized with 0s.
+ * Caller is responsible for:
+ *   later calling grid_delete.
+ */
+grid_t* grid_new();
+
+/**************** grid_get ****************/
+/* Return the number at the given row and column.
+ *
+ * Caller provides:
+ *   valid pointer to grid, integers representing the row and column.
+ * We return:
+ *   the number at the given row and column if valid row and col;
+ *   -1 if grid is NULL or row or col is not an integer from 0 to 9.
+ * Notes:
+ *   the grid is unchanged by this operation.
+ */
+int grid_get(grid_t* grid, int row, int col);
+
+/**************** grid_set ****************/
+/* Sets the number at the given row and column to the given number.
+ *
+ * Caller provides:
+ *   valid pointer to grid, integers representing the row and column.
+ * We return:
+ *   false if grid is NULL or row or col is not an integer from 0 to 9,
+ *   true iff new number was set.  
+ */
+bool grid_set(grid_t* grid, int row, int col, int num);
+
+/**************** grid_print ****************/
+/* Print the whole table; provide the output file and func to print each item.
+ * 
+ * Caller provides:
+ *   valid pointer to grid, 
+ *   FILE open for writing.
+ * We print:
+ *   nothing, if NULL fp.
+ *   "(null)" if NULL grid.
+ *   otherwise, 9 lines representing the grid, with each line
+ *   containing 9 integers separated by whitespace.
+ * Note:
+ *   the grid and its contents are not changed by this function,
+ */
+void grid_print(grid_t* grid, FILE *fp);
+
+/**************** grid_delete ****************/
+/* Delete grid.
+ *
+ * Caller provides:
+ *   valid grid pointer.
+ * We do:
+ *   if grid==NULL, do nothing.
+ *   otherwise, free the grid.
+ */
+void grid_delete(grid_t* grid);
+
+/**************** valid_grid ****************/
+/* Determines if a grid is a valid sudoku grid.
+ *
+ * Caller provides:
+ *   valid grid pointer.
+ * We do:
+ *   if grid==NULL, do nothing.
+ *   otherwise, check that each number from 1 to 9 appears at most
+ *   once in every row, column, and 3x3 square region.
+ * We return:
+ *   pointer to the new index; return NULL if error.
+ */
+bool valid_grid(grid_t* grid);
+
+/**************** load_grid ****************/
+/* Loads a file and stores its contents in a new grid
+ *
+ * Caller provides:
+ *   FILE open for reading.
+ * We do:
+ *   if filename==NULL, return NULL.
+ *   otherwise, read the contents of the file and store it in a new grid.
+ * We return:
+ *   NULL if error.
+ *   NULL if the contents of the file do not have valid syntax for
+ *   a sudoku grid (must contain 9 lines, with each line containing
+ *   exactly 9 integers from 0 to 9, separated by whitespace).
+ *   NULL if the grid is not a valid sudoku grid (use valid_grid).
+ *   otherwise, the pointer to the new grid.
+ * Caller is responsible for:
+ *   later calling grid_delete.
+ */
+grid_t* load_grid(FILE *fp);
+
+#endif // __GRID_H
