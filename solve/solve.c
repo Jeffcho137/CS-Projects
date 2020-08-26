4c17cf115e1f21e6e0817d43d73572061507abd9
Jeffrey J. Cho 22 <jeffcho137@flume.cs.dartmouth.edu>
2020-08-26 11:58:28 -0400

    solve updated

--- solve/solve.c
+++ solve/solve.c
@@ -11,6 +11,82 @@
 #include "../common/grid.h"
 //#include <time.h>
 
+/********** check_row **********/
+/*
+ * checks if a number exists in a row
+ */
+
+static bool check_row(grid_t *grid, int row, int col, int value)
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (i == col)
+			continue;
+		if (grid_get(row, i) == value)
+			return true;
+	}
+	return false;
+}
+
+/********** check_col **********/
+/*
+ * checks if a number exists in a column
+ */
+static bool check_col(grid_t *grid, int row, int col, int value)
+{
+	for (int i = 0; i < 9; i ++)
+	{
+		if (i == row)
+			continue;
+
+		if (grid_get(grid, i, col) == value)
+			return true;
+	}
+
+	return false;
+}
+
+/********** check_board **********/
+/*
+ * checks if a number exists in a smaller divided board (9 X 9) --> (3 X 3)
+ */
+
+static bool check_board(grid_t *grid, int row, int col, int value)
+{
+	int beg_row = (int)(row/3) * 3;
+	int beg_col = (int)(col/3) * 3;
+
+	for (int i = beg_row; i < beg_row + 3; i++)
+	{
+		for (int j = beg_col; j < beg_col + 3; j++)
+		{
+			if (i == row && j == col)
+				continue;
+			if (grid_get(grid, i, j) == value)
+				return true;
+		}
+	}
+
+	return false;
+}
+
+
+/********** number_present **********/
+/*
+ * tries to see if a number already exists in a cell
+ */
+
+static bool number_present(grid_t *grid, int row, int col, int value)
+{
+	if (check_row(grid, row, col, value))
+		return true;
+	if (check_col(grid, row, col, vlaue))
+		return true;
+	if (check_board(grid, row, col, value))
+		return true;
+	return false;
+}
+
 /********** check_unique() **********/
 /* see solve.h for more info */
 int check_unique(grid_t *grid, int row, int col, int num)
@@ -28,7 +104,7 @@ int check_unique(grid_t *grid, int row, int col, int num)
 	
 	for (int i = 1; i <= 9 && num < 2; ++i)
 	{
-		if(!valid_num(grid, row, col))
+		if(!number_present(grid, row, col, i))
 		{
 			grid_set(grid, row, col, i);
 			num = check_unique(grid, row, col + 1, num);
@@ -90,7 +166,7 @@ static bool solve_board(grid_t *grid, int start_pos)
 	int value = 1;
 	
 
-	while (valid_num(grid, row, col) || !solve_board(grid, start_pos + 1))
+	while (number_present(grid, row, col, value) || !solve_board(grid, start_pos + 1))
 	{
 		grid_set(grid, row, col, ++value); 
 		//b->grid[row][col] = ++value;
