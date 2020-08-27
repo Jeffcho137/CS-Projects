56b9084ddb0b9d0ba39bb2681690bf4fdb08937c
Samuel Y. Wang 23 <samuelwang@flume.cs.dartmouth.edu>
2020-08-27 06:00:04 -0400

    update create

--- create/createtest.c
+++ create/createtest.c
@@ -11,14 +11,11 @@
 #include <string.h>
 #include <time.h>
 #include "create.h"
-#include "grid.h"
 #include "solve.h"
 
 /* **************************************** */
 int main() 
-{
- srand(time(NULL));
- 
+{ 
  // create puzzle
  printf("creating new puzzle\n");
  grid_t grid = create_puzzle();
@@ -43,36 +40,7 @@ int main()
  printf("printing the puzzle\n\n");
  grid_print(grid, stdout);
 
-
- 
- 
- // repeat with another puzzle
- printf("\nrepeating process with another puzzle\n\n");
+ // delete the puzzle
  grid_delete(grid);
 
- // create puzzle
- printf("creating new puzzle\n");
- grid = create_puzzle();
-
- // check whether the puzzle is valid
- valid = valid_grid(grid);
-
- if (valid) {
-   printf("puzzle is valid\n");
- } else {
-   printf("puzzle is not valid\n");
- }
-
- // check whether the puzzle has a unique solution
- if (check_unique(grid, 0, 0, 0) == 1) {
-   printf("puzzle has a unique solution\n");
- } else {
-   printf("puzzle does not have a unique solution\n");
- }
-
- // print the puzzle
- printf("printing the puzzle\n\n");
- grid_print(grid, stdout);
-
- grid_delete(grid);
 }
